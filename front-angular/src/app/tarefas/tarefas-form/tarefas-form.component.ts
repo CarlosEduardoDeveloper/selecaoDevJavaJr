1a60b72f38180109b7f825e7e6f6b1542f6f4f73
carlos.eduardo <https://github.com/CarlosEduardoDeveloper/BVAtividades.git>
2022-10-05 19:26:14 -0300

    adicao snackbar

--- front-angular/src/app/tarefas/tarefas-form/tarefas-form.component.ts
+++ front-angular/src/app/tarefas/tarefas-form/tarefas-form.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup } from '@angular/forms';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { TarefasService } from '../service/tarefas.service';
 
 @Component({
   selector: 'app-tarefas-form',
@@ -10,7 +12,9 @@ export class TarefasFormComponent implements OnInit {
 
 form: FormGroup;
 
-  constructor(private formBuilder: FormBuilder) {
+  constructor(private formBuilder: FormBuilder,
+    private service: TarefasService,
+    private snackBar: MatSnackBar) {
     this.form = this.formBuilder.group({
       descricao: [null],
       atividadeConcluida: [null]
@@ -20,8 +24,15 @@ form: FormGroup;
   ngOnInit(): void {
   }
 
-  onSubmit(){}
+  onSubmit(){
+    this.service.salvar(this.form.value)
+    .subscribe(result => console.log(result), error => this.onError());
+  }
 
   onCancel(){}
+
+  private onError(){
+    this.snackBar.open('Erro ao salvar atividade', '', {duration: 5000})
+  }
 
 }
